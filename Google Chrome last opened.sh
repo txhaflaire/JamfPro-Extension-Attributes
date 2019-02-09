2849157c63991e0d47b821d940b0e16e898838c3
Thijs Xhaflaire <txhaflaire@bol.com>
2019-02-09 11:23:33 +0100

    Add files via upload

---
+++ Google Chrome last opened.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+apppath="/Applications/Google Chrome.app"
+usedate=$(/usr/bin/mdls -name kMDItemLastUsedDate "$apppath" | /usr/bin/awk '{print $3, $4}')
+
+if [ "$usedate" == "(null)" ]
+then
+	result="2001-01-01 01:01:01"
+else
+	result="$usedate"
+fi
+
+echo "<result>$result</result>"
